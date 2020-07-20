da3a27bbbdad15cf8d937ac9a3f49fc596922406
gustavofortti <gustavofortti@gmail.com>
2020-07-19 21:36:47 -0300

    Node-api estrutura inicial

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.get('/', (req, res) => {
+    res.send("Hello Roketseat");
+});
+
+app.listen(3001);
